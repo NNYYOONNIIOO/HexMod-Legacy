dc39242c255260721b905e8146695f994ff5cf30
NNYYOONNIIOO <wubidaxiang@163.com>
2026-09-11 21:23:09 +0800

    fix: repair guide book registry initialization

--- src/main/java/at/petra_k/hexcasting/common/item/ItemHexGuideBook.java
+++ src/main/java/at/petra_k/hexcasting/common/item/ItemHexGuideBook.java
@@ -1,30 +1,31 @@
 package at.petra_k.hexcasting.common.item;
 
+import at.petra_k.hexcasting.api.HexAPI;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
-import vazkii.patchouli.common.item.ItemModBook;
+import vazkii.patchouli.common.item.PatchouliItems;
 
 /** Patchouli 1.12.2 guide-book item for the Hex Casting port. */
-public final class ItemHexGuideBook extends ItemModBook {
+public final class ItemHexGuideBook extends Item {
     private static final String BOOK_TAG = "patchouli:book";
     private static final String BOOK_ID = "hexcasting:hexcasting";
 
     public ItemHexGuideBook() {
-        super();
-        setRegistryName("hexcasting", "guide_book");
-        setUnlocalizedName("hexcasting.guide_book");
         setMaxStackSize(1);
+        setRegistryName(HexAPI.MOD_ID, "guide_book");
+        setUnlocalizedName("hexcasting.guide_book");
     }
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
         ItemStack stack = player.getHeldItem(hand);
         ensureBookTag(stack);
-        return super.onItemRightClick(world, player, hand);
+        return PatchouliItems.book.onItemRightClick(world, player, hand);
     }
 
     private static void ensureBookTag(ItemStack stack) {
