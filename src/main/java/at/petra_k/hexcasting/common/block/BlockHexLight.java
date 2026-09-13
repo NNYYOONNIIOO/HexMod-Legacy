7c78c099ba578ed4db6025dd864263c824986619
NNYYOONNIIOO <wubidaxiang@163.com>
2026-09-13 14:02:38 +0800

    fix: use 1.12 resource path API

--- src/main/java/at/petra_k/hexcasting/common/block/BlockHexLight.java
+++ src/main/java/at/petra_k/hexcasting/common/block/BlockHexLight.java
@@ -3,6 +3,9 @@ package at.petra_k.hexcasting.common.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.IBlockAccess;
@@ -39,5 +42,25 @@ public final class BlockHexLight extends Block {
     @Override
     public boolean isFullCube(IBlockState state) {
         return false;
+    }
+
+    private boolean isPaperLantern() {
+        return getRegistryName() != null
+            && getRegistryName().getResourcePath().contains("paper");
+    }
+
+    @Override
+    public boolean isFlammable(IBlockAccess world, BlockPos pos, EnumFacing face) {
+        return isPaperLantern();
+    }
+
+    @Override
+    public int getFireSpreadSpeed(IBlockAccess world, BlockPos pos, EnumFacing face) {
+        return isPaperLantern() ? 100 : 0;
+    }
+
+    @Override
+    public int getFlammability(IBlockAccess world, BlockPos pos, EnumFacing face) {
+        return isPaperLantern() ? 60 : 0;
     }
 }
